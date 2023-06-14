3e34defd38b0775d7462ba4d8a7b7a62ce33aa93
Josh Burns <joshyburnss@gmail.com>
2023-06-14 19:04:07 +1000

    use rich to create a progress bar for downloads

--- chanscrape.py
+++ chanscrape.py
@@ -2,6 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import argparse
+from rich.progress import Progress
 
 
 def get_media_urls_from_4chan_thread(url):
@@ -18,13 +19,20 @@ def get_media_urls_from_4chan_thread(url):
 
 def download_media_from_urls(urls, output_dir):
     os.makedirs(output_dir, exist_ok=True)
-    for i, url in enumerate(urls, start=1):
-        filename = url.split("/")[-1]
-        output_path = os.path.join(output_dir, filename)
-        response = requests.get(url)
-        with open(output_path, "wb") as f:
-            f.write(response.content)
-        print(f"Downloaded media {i}/{len(urls)}: {filename}")
+    with Progress() as progress:
+        task = progress.add_task("[cyan]Downloading media...", total=len(urls))
+        for i, url in enumerate(urls, start=1):
+            filename = url.split("/")[-1]
+            output_path = os.path.join(output_dir, filename)
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+            total_size = int(response.headers.get("content-length", 0))
+            progress.update(task, completed=i, description=f"Downloading {filename}", total=total_size)
+            with open(output_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+            progress.update(task, advance=1)
+    print("[green]Download completed!")
 
 
 if __name__ == "__main__":
